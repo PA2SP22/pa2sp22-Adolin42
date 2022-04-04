07308fc9f387340a0a90e9940c70977ada94fbc8
Ubuntu <ubuntu@ip-172-31-85-137.ec2.internal>
2022-04-04 18:33:18 +0000

    WIP Lab #14 (2)

--- labs/14/lab_14.cpp
+++ labs/14/lab_14.cpp
@@ -1,6 +1,6 @@
 /*
  * Name        : lab_14.cpp
- * Author      : FILL IN
+ * Author      : Ptolemy Linden
  * Description : Working with Bubble and Selection Sort
  */
 #include <iostream>
@@ -69,13 +69,79 @@ int main() {
 
 // CODE HERE -- FUNCTION DEFINITION
 
+/*
+ * Apply the bubble sort algorithm to sort an array of integers.
+ * @param int[] the_array - The integer array to be sorted
+ * @param unsigned int size - The size of the_array
+ * @return int - The number of passes the algorithm does. In this case "pass" is
+ *               defined as the number of times the outside loop runs. You
+ *               should increment your variable once you enter the outside loop.
+ */
+int BubbleSort(int the_array[], unsigned int size) {
+  int passes = 0;
+  
+  for (int i = size-1; i >= 1; i--) {
+    passes++;
+      for (int j = 0; i >= i-1; i++) {
+        if (the_array[j] > the_array[j+1]) {
+          SwapValues(the_array[j], the_array[j+1]);
+        }
+    }
+  }
+  return passes;
+}
 
+/*
+ * Apply the optimized bubble sort algorithm to sort an array of integers.
+ * @param int[] the_array - The integer array to be sorted
+ * @param unsigned int size - The size of the_array
+ * @return int - The number of passes the algorithm does. In this case "pass" is
+ *               defined as the number of times the outside loop runs. You
+ *               should increment your variable once you enter the outside loop.
+ */
+int OptimizedBubbleSort(int the_array[], unsigned int size) {
+ 
+}
+
+/*
+ * Apply the selection sort algorithm to sort an array of integers.
+ * @param int[] the_array - The integer array to be sorted
+ * @param unsigned int size - The size of the_array
+ * @return int - The number of passes the algorithm does. In this case "pass" is
+ *               defined as the number of times the outside loop runs. You
+ *               should increment your variable once you enter the outside loop.
+ */
+int SelectionSort(int the_array[], unsigned int size) {
+  unsigned int passes = 0;
+  for (unsigned int i = 0; i <= size-1; i++) {
+    passes++;
+    unsigned int index_of_smallest = i;
+    for (unsigned int j = i+1; j <= size-1; j++) {
+      if (the_array[j] < the_array[index_of_smallest]) {
+        index_of_smallest = j;
+      }
+      if (index_of_smallest != i) {
+        SwapValues(the_array[i], the_array[index_of_smallest]);
+      }
+    }
+  }
+  return passes;
+}
+
+/*
+ * Swap the values of two integer variables.
+ * @param int &value_1 - The first value to be swapped.
+ * @param int &value_2 - The second value to be swapped.
+ */
 void SwapValues(int &value_1, int &value_2) {
   // DO NOT ALTER THE NEXT 3 LINES!!!
   if (GRADER) {
     cout << value_1 << " " << value_2 << endl;
   }
   // Code SWAP Algorithm Here
+  int swap = value_1;
+  value_1 = value_2;
+  value_2 = swap;
 }
 
 // For testing (DO NOT ALTER)
